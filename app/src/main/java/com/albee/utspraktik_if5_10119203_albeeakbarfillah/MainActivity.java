96c34bf6d971f854d13f2392dc95187b7b5b5934
albeefillah <albeefillah17@gmail.com>
2022-06-02 17:40:16 +0700

    commit pertama

---
+++ app/src/main/java/com/albee/utspraktik_if5_10119203_albeeakbarfillah/MainActivity.java
@@ -0,0 +1,14 @@
+package com.albee.utspraktik_if5_10119203_albeeakbarfillah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
